ba948b02b8648c556bdde55513ac0365f167fb34
Yiorgis Gozadinos <ggozad@crypho.com>
2012-10-22 17:07:58 +0200

    Fix and generalize filtering

--- behaving/fsinspector.py
+++ behaving/fsinspector.py
@@ -1,11 +1,13 @@
 import os
+import time
 from shutil import rmtree
 
 
 class FSInspector(object):
 
-    def __init__(self, path):
+    def __init__(self, path, timeout=5):
         self.path = path
+        self.timeout = timeout
 
     def messages_for_user(self, user):
         user_path = os.path.join(self.path, user)
@@ -22,6 +24,16 @@ class FSInspector(object):
             path = os.path.join(user_path, path)
             with open(path, 'r') as f:
                 messages.append(f.read())
+        return messages
+
+    def user_messages(self, user, f=None):
+        messages = []
+        start = time.time()
+        while time.time() - start < self.timeout:
+            messages = filter(f, self.messages_for_user(user))
+            if messages:
+                break
+            time.sleep(0.2)
         return messages
 
     def clear(self):
