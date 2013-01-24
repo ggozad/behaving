8cfc1ce31a2018ad686e699b5c08e8684eef14db
Yiorgis Gozadinos <ggozad@crypho.com>
2013-01-24 12:06:10 +0100

    Fix click link by partial text

--- src/behaving/web/steps/links.py
+++ src/behaving/web/steps/links.py
@@ -23,4 +23,13 @@ def click_link_with_text(context, text):
 @when(u'I click the link with text that contains "{text}"')
 @persona_vars
 def click_link_with_text_that_contains(context, text):
-    context.browser.click_link_by_partial_text(text)
+
+    # Thought the following search by xpath should work
+    # find_by_xpath("//a[contains(text(), '%s')]" % text)
+    # but it doesn't.
+    # The following is a workaround
+    anchors = context.browser.find_link_by_partial_text(text) or \
+        [a for a in context.browser.find_by_tag('a') if text in a.text]
+
+    assert anchors, 'Link not found'
+    anchors[0].click()
