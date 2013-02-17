d40fdbe482622392fe833793cdc3f792eec97b2a
Yiorgis Gozadinos <ggozad@crypho.com>
2013-02-17 14:40:26 -0800

    header

--- src/behaving/sms/mock.py
+++ src/behaving/sms/mock.py
@@ -34,6 +34,7 @@ class SMSServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
         # body = params.get('msg')
 
         self.send_response(200)
+        self.send_header("Content-type:", "text/json")
         self.end_headers()
         self.wfile.write('{"messages":[{"status":"0"}]}')
         global output_dir
