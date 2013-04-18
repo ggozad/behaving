8cd58eef0062e93487f86c533c07f7c1adbac444
Yiorgis Gozadinos <ggozad@crypho.com>
2013-04-18 10:15:04 +0200

    Add persona variable decorator to all forms matcher

--- src/behaving/web/steps/forms.py
+++ src/behaving/web/steps/forms.py
@@ -23,11 +23,13 @@ def i_choose_in_radio(context, name, value):
 
 
 @step(u'I check "{name}"')
+@persona_vars
 def i_check(context, name):
     context.browser.check(name)
 
 
 @step(u'I uncheck "{name}"')
+@persona_vars
 def i_uncheck(context, name):
     context.browser.uncheck(name)
 
@@ -39,6 +41,7 @@ def i_select(context, value, name):
 
 
 @step(u'I press "{name}"')
+@persona_vars
 def i_press(context, name):
     button = \
         context.browser.find_by_id(name) or \
@@ -52,6 +55,7 @@ def i_press(context, name):
 
 
 @step(u'I press the element with xpath "{xpath}"')
+@persona_vars
 def i_press_xpath(context, xpath):
     button = context.browser.find_by_xpath(xpath)
     assert button, u'Element not found'
@@ -69,11 +73,13 @@ def i_attach(context, name, path):
 
 
 @step('I set the inner HTML of the element with id "{id}" to "{contents}"')
+@persona_vars
 def set_html_content_to_element_with_id(context, id, contents):
     assert context.browser.evaluate_script("document.getElementById('%s').innerHTML = '%s'" % (id, contents)), u'Element not found or could not set HTML content'
 
 
 @step('I set the inner HTML of the element with class "{klass}" to "{contents}"')
+@persona_vars
 def set_html_content_to_element_with_class(context, klass, contents):
     assert context.browser.evaluate_script("document.getElementsByClassName('%s')[0].innerHTML = '%s'" % (klass, contents)), u'Element not found or could not set HTML content'
 
@@ -81,30 +87,30 @@ def set_html_content_to_element_with_class(context, klass, contents):
 @step(u'field "{name}" should have the value "{value}"')
 @persona_vars
 def field_has_value(context, name, value):
-    el = context.browser.find_by_id(name) or \
-         context.browser.find_by_name(name)
+    el = context.browser.find_by_id(name) or context.browser.find_by_name(name)
     assert el, u'Element not found'
     assert el.first.value == value, "Values do not match"
 
 
 @step(u'"{name}" should be enabled')
+@persona_vars
 def is_enabled(context, name):
-    el = context.browser.find_by_id(name) or \
-         context.browser.find_by_name(name)
+    el = context.browser.find_by_id(name) or context.browser.find_by_name(name)
     assert el, u'Element not found'
     assert el.first._element.is_enabled()
 
 
 @step(u'"{name}" should be disabled')
 @step(u'"{name}" should not be enabled')
+@persona_vars
 def is_disabled(context, name):
-    el = context.browser.find_by_id(name) or \
-         context.browser.find_by_name(name)
+    el = context.browser.find_by_id(name) or context.browser.find_by_name(name)
     assert el, u'Element not found'
     assert not el.first._element.is_enabled()
 
 
 @step(u'field "{name}" should be valid')
+@persona_vars
 def field_is_valid(context, name):
     assert context.browser.find_by_name(name), u'Element not found'
     assert context.browser.evaluate_script("document.getElementsByName('%s')[0].checkValidity()" % name), \
@@ -113,6 +119,7 @@ def field_is_valid(context, name):
 
 @step(u'field "{name}" should be invalid')
 @step(u'field "{name}" should not be valid')
+@persona_vars
 def field_is_invalid(context, name):
     assert context.browser.find_by_name(name), u'Element not found'
     assert not context.browser.evaluate_script("document.getElementsByName('%s')[0].checkValidity()" % name), \
