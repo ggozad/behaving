539f26d13b88de804a28339fc09093f89d8c498a
Ross Elliott <ross@wearthefoxhat.com>
2014-06-17 09:54:02 +0100

    Make persona vars avaiable to more steps

--- src/behaving/web/steps/basic.py
+++ src/behaving/web/steps/basic.py
@@ -5,58 +5,69 @@ from behaving.personas.persona import persona_vars
 
 
 @step(u'I wait for {timeout:d} seconds')
+@persona_vars
 def wait_for_timeout(context, timeout):
     time.sleep(timeout)
 
 
 @step(u'I show the element with id "{id}"')
+@persona_vars
 def show_element_by_id(context, id):
     assert context.browser.find_by_id(id)
     context.browser.execute_script('document.getElementById("%s").style.display="inline";' % id)
 
 
 @step(u'I hide the element with id "{id}"')
+@persona_vars
 def hide_element_by_id(context, id):
     assert context.browser.find_by_id(id)
     context.browser.execute_script('document.getElementById("%s").style.display="none";' % id)
 
 
 @step(u'I should see "{text}"')
+@persona_vars
 def should_see(context, text):
     assert context.browser.is_text_present(text), u'Text not found'
 
 
 @step(u'I should not see "{text}"')
+@persona_vars
 def should_not_see(context, text):
     assert context.browser.is_text_not_present(text), u'Text was found'
 
 
 @step(u'I should see "{text}" within {timeout:d} seconds')
+@persona_vars
 def should_see_within_timeout(context, text, timeout):
     assert context.browser.is_text_present(text, wait_time=timeout), u'Text not found'
 
 
 @step(u'I should not see "{text}" within {timeout:d} seconds')
+@persona_vars
 def should_not_see_within_timeout(context, text, timeout):
     assert context.browser.is_text_not_present(text, wait_time=timeout), u'Text was found'
 
 
 @step(u'I should see an element with id "{id}"')
+@persona_vars
 def should_see_element_with_id(context, id):
     assert context.browser.is_element_present_by_id(id), u'Element not present'
 
 
 @step(u'I should not see an element with id "{id}"')
+@persona_vars
 def should_not_see_element_with_id(context, id):
     assert context.browser.is_element_not_present_by_id(id), u'Element is present'
 
 
 @step(u'I should see an element with id "{id}" within {timeout:d} seconds')
+@persona_vars
 def should_see_element_with_id_within_timeout(context, id, timeout):
     assert context.browser.is_element_present_by_id(id, wait_time=timeout), u'Element not present'
 
 
 @step(u'I should not see an element with id "{id}" within {timeout:d} seconds')
+@persona_vars
 def should_not_see_element_with_id_within_timeout(context, id, timeout):
     assert context.browser.is_element_not_present_by_id(id, wait_time=timeout), u'Element is present'
 
