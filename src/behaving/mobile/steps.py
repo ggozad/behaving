496f02d7f282f23804d2b7ca489ebc2bf99c501d
Yiorgis Gozadinos <yg@mcash.no>
2014-07-22 11:26:16 +0200

    Start the ios simulator

--- src/behaving/mobile/steps.py
+++ src/behaving/mobile/steps.py
@@ -1,2 +1,18 @@
+import os
+from appium import webdriver
+
 from behave import step
-from behaving.personas.persona import persona_vars
+
+
+@step('an iOS simulator running "{name}"')
+def given_an_ios_simulator(context, name):
+
+    app_path = os.path.join(context.app_dir, name)
+    context.iosdriver = webdriver.Remote(
+        command_executor=context.webdriver_url,
+        desired_capabilities={
+            'app': app_path,
+            'platformName': 'iOS',
+            'platformVersion': '7.1',
+            'deviceName': 'iPhone Simulator'
+        })
