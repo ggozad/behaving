a6e1c3013ebebe6c87cd4022c561b475593f91a0
Erlend Halvorsen <erlendfh@gmail.com>
2014-07-23 13:54:56 +0200

    iOS Test App

---
+++ src/behaving/tests/apps/Behaving Test App/Behaving Test App/TAViewController.m
@@ -0,0 +1,27 @@
+//
+//  TAViewController.m
+//  Behaving Test App
+//
+//  Created by Erlend Halvorsen on 23/07/14.
+//  Copyright (c) 2014 Behaving. All rights reserved.
+//
+
+#import "TAViewController.h"
+
+@interface TAViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UISwitch *aSwitch;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *scrollViewLabel;
+
+@end
+
+@implementation TAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
---
+++ src/behaving/tests/apps/Behaving Test App/Behaving Test AppTests/Behaving_Test_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Behaving_Test_AppTests.m
+//  Behaving Test AppTests
+//
+//  Created by Erlend Halvorsen on 23/07/14.
+//  Copyright (c) 2014 Behaving. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Behaving_Test_AppTests : XCTestCase
+
+@end
+
+@implementation Behaving_Test_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
