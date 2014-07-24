710846441dc067728cec60fa0892b22c1407550c
Yiorgis Gozadinos <yg@mcash.no>
2014-07-24 15:03:08 +0200

    lock/install/uninstall/lauch/close

--- src/behaving/mobile/steps.py
+++ src/behaving/mobile/steps.py
@@ -3,6 +3,7 @@ from urllib2 import URLError
 
 from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
+from selenium.common.exceptions import WebDriverException
 from behave import step
 
 
@@ -31,6 +32,11 @@ def given_an_android_simulator(context, name):
         assert False, 'Appium is not running on the specified webdriver_url'
 
 
+@step('I lock the device for {timeout:d} seconds')
+def lock_device(context, timeout):
+    context.device.lock(timeout)
+
+
 @step('I tap "{name}" and drag to "{coords}"')
 def drag_name_to_coords(context, name, coords):
     coords = eval(coords)
@@ -38,3 +44,43 @@ def drag_name_to_coords(context, name, coords):
     action = TouchAction(context.device)
     action.press(el).move_to(x=0, y=0).move_to(x=100, y=0).move_to(x=0, y=100).release()
     action.perform()
+
+
+@step('I install the app "{name}"')
+def install_application(context, name):
+    # Does not work on iOS
+    app_path = os.path.join(context.app_dir, name)
+    try:
+        context.device.install_app(app_path)
+    except WebDriverException, e:
+        assert False, e.msg
+
+
+@step('I remove the app "{uid}"')
+def remove_application(context, uid):
+    # Does not work on iOS
+    try:
+        context.device.remove_app(uid)
+    except WebDriverException, e:
+        assert False, e.msg
+
+
+@step('I launch the app')
+def launch_app(context):
+    try:
+        context.device.launch_app()
+    except WebDriverException, e:
+        assert False, e.msg
+
+
+@step('I close the app')
+def close_app(context):
+    try:
+        context.device.close_app()
+    except WebDriverException, e:
+        assert False, e.msg
+
+
+@step('the application "{uid}" is installed')
+def application_is_installed(context, uid):
+    assert context.device.is_app_installed(uid), 'Application %s is not installed' % uid
--- src/behaving/tests/features/ios.feature
+++ src/behaving/tests/features/ios.feature
@@ -10,3 +10,12 @@ Feature: iOS support
         And I press "Calculate"
         Then I should see "84"
         # When I press "toggleCalculate"
+
+    @ios
+    @mobile
+    Scenario: Application is installed
+        Given an iOS simulator running "ios_test_app/build/Release-iphonesimulator/ios_test_app.app"
+        When I close the app
+        And I launch the app
+        And I lock the device for 3 seconds
+        #Then the application "com.behaving.test.app.ios-test-app" is installed
