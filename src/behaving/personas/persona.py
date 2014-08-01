191b256c4805aa6954b28ff7ae4eaae78c318f53
Erlend Halvorsen <erlendfh@gmail.com>
2014-08-01 13:38:07 +0200

    Added support for $persona.variables.with.multiple.segments

--- src/behaving/personas/persona.py
+++ src/behaving/personas/persona.py
@@ -8,7 +8,7 @@ class Persona(dict):
     pass
 
 
-var_exp = re.compile('\$(\w*)')
+var_exp = re.compile('\$(\w+(?:\.\w+)*)')
 
 
 class PersonaVarMatcher(object):
@@ -17,14 +17,22 @@ class PersonaVarMatcher(object):
         self.func = func
 
     def replace(self, *args, **kwargs):
-
+        import pdb
         context = args[0]
         if hasattr(context, 'persona'):
             for kwname, kwvalue in kwargs.items():
                 variables = var_exp.findall(str(kwvalue))
                 for var in variables:
-                    if var in context.persona:
-                        kwargs[kwname] = kwargs[kwname].replace('$' + var, context.persona[var])
+                    components = var.split(".")
+                    current = context.persona
+                    for component in components:
+                        if hasattr(current, component):
+                            current = getattr(current, component)
+                        else:
+                            current = current[component]
+
+                    if current != context.persona:
+                        kwargs[kwname] = kwargs[kwname].replace('$' + var, current)
 
         self.func.__call__(*args, **kwargs)
 
