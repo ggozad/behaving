1d1dd96ac604ae14656589930a47b428804838f9
Yiorgis Gozadinos <yg@mcash.no>
2014-08-12 12:29:34 +0200

    Unlock android device before running tests

--- src/behaving/mobile/environment.py
+++ src/behaving/mobile/environment.py
@@ -1,8 +1,16 @@
+import os
+import subprocess
 from . import setup, teardown
 
 
 def before_all(context):
-    pass
+    # Unlock the android emulator so that we can install and run an app
+    android_home = os.environ.get('ANDROID_HOME')
+    if android_home:
+        try:
+            subprocess.call([os.path.join(android_home, 'platform-tools', 'adb'), 'shell', 'input', 'keyevent', '82'])
+        except OSError:
+            pass
 
 
 def before_feature(context, feature):
