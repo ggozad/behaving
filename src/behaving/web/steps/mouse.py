1418c5148127d604f84e08d1056cd91465c9597c
Yiorgis Gozadinos <ggozad@crypho.com>
2015-09-23 13:53:48 +0200

    Mouse over/out an element

---
+++ src/behaving/web/steps/mouse.py
@@ -0,0 +1,18 @@
+from behave import step
+
+from behaving.personas.persona import persona_vars
+
+
+@step(u'I mouse over the element with xpath "{xpath}"')
+@persona_vars
+def mouse_over(context, xpath):
+    element = context.browser.find_by_xpath(xpath)
+    assert element, u'Element not found'
+    element.mouse_over()
+
+
+@step(u'I mouse out of the element with xpath "{xpath}"')
+def mouse_out(context, xpath):
+    element = context.browser.find_by_xpath(xpath)
+    assert element, u'Element not found'
+    element.mouse_out()
