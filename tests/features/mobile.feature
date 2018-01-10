3bc1b5ae7d34ae7d9d39d61f67269c3bc18b9748
Yiorgis Gozadinos <ggozad@crypho.com>
2018-01-10 16:00:18 +0100

    Handle buttons

--- tests/features/mobile.feature
+++ tests/features/mobile.feature
@@ -3,4 +3,8 @@ Feature: Mobile
     @mobile
     Scenario: Use the ios simulator
         Given an iOS simulator running "ios/build/Build/Products/Debug-iphonesimulator/rntest.app"
-        Then I should see "Welcome to React Native!"
+        Then I should see "Behaving mobile tests"
+        When I press "Normal button"
+        Then I should see "Normal button pressed"
+        When I press "Touchable opacity"
+        Then I should see "Touchable opacity pressed"
--- tests/rntest/App.js
+++ tests/rntest/App.js
@@ -4,34 +4,47 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Button,
+  TouchableOpacity
+} from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = { feedback: "" };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.welcome}>Behaving mobile tests</Text>
+        <Text>{this.state.feedback}</Text>
+        <Button
+          title="Normal button"
+          onPress={() => {
+            this.setState({ feedback: "Normal button pressed" });
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({ feedback: "Touchable opacity pressed" });
+          }}
+          accessibilityLabel="Touchable opacity"
+        >
+          <Text>Touchable Opacity</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -40,18 +53,18 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
