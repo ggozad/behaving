bd705880bdf9bd1bb632307644687105eb6f9bd2
Yiorgis Gozadinos <ggozad@crypho.com>
2018-01-17 11:30:06 +0100

    Make things work with named browsers

--- src/behaving/mobile/__init__.py
+++ src/behaving/mobile/__init__.py
@@ -1,5 +1,7 @@
 # Generic setup/teardown for compatibility with pytest et al.
+from .ios import *
+
+
 def setup(context):
     if not hasattr(context, 'mobile_app_dir'):
         context.mobile_app_dir = '/'
-    context.mobile_app_cache = {}
