7fa0612015663016a69a20910fd1ca8a49ad4703
Yiorgis Gozadinos <ggozad@crypho.com>
2018-01-29 11:46:50 +0100

    Find element by ios class chain

--- src/behaving/mobile/steps/basic.py
+++ src/behaving/mobile/steps/basic.py
@@ -6,8 +6,13 @@ def see_accessibility_id(context, id):
     assert context.browser.find_by_accessibility_id(id)
 
 
+@step(u'I should see an element with iOS class chain "{chain}"')
+def see_ios_class_chain(context, chain):
+    assert context.browser.find_by_ios_class_chain(chain), u'Element not found'
+
+
 @step(u'I press the element with iOS class chain "{chain}"')
-def press_class_chain(context, chain):
+def press_ios_class_chain(context, chain):
     el = context.browser.find_by_ios_class_chain(chain)
     assert el, u'Element not found'
     el.first.click()
