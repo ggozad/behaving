093ffd1ec7748027e840bef4eaeadcb4e056d5a8
Yiorgis Gozadinos <ggozad@crypho.com>
2022-09-27 08:27:08 +0200

    Fix switch_to.alert in selenium 4

--- src/behaving/web/steps/alerts.py
+++ src/behaving/web/steps/alerts.py
@@ -48,20 +48,20 @@ def alert_contains_text_timeout(context, text, timeout):
 @step(u'I enter "{text}" to the alert')
 @persona_vars
 def set_alert_text(context, text):
-    alert = context.browser.driver.switch_to_alert()
+    alert = context.browser.driver.switch_to.alert
     assert alert, u"Alert not found"
     alert.send_keys(text)
 
 
 @step(u"I accept the alert")
 def accept_alert(context):
-    alert = context.browser.driver.switch_to_alert()
+    alert = context.browser.driver.switch_to.alert
     assert alert, u"Alert not found"
     alert.accept()
 
 
 @step(u"I dismiss the alert")
 def dimiss_alert(context):
-    alert = context.browser.driver.switch_to_alert()
+    alert = context.browser.driver.switch_to.alert
     assert alert, u"Alert not found"
     alert.dismiss()
